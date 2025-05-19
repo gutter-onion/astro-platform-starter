d55774bf9458f6985b274bb4d888052f08309a75
gutter-onion <chrismhoran@icloud.com>
2025-05-19 12:02:10 -0400

    Update stackbit.config.js

--- stackbit.config.js
+++ stackbit.config.js
@@ -4,14 +4,7 @@ export default {
   stackbitVersion: '~0.6.0',
   ssgName: 'nextjs',
   nodeVersion: '16',
-  contentSources: [
-    new ContentfulContentSource({
-      spaceId: process.env.CONTENTFUL_SPACE_ID,
-      environment: process.env.CONTENTFUL_ENVIRONMENT,
-      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
-      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
-    }),
-  ],
+  contentSources: [],
   models: {
     page: { type: 'page', urlPath: '/{slug}' },
   },
