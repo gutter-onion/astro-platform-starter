66259f2336b30a533e60fb566a4b6cff581a748e
gutter-onion <chrismhoran@icloud.com>
2025-05-19 12:10:13 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,3 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  server: {
-    allowedHosts: [
-      'devserver-main--ctrlaltrecycle.netlify.app', // Add your Netlify subdomain here
-    ],
-  },
-})
+server: {
+  allowedHosts: 'all',
+}
